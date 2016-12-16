755dccd3b7ce749bfabe3c3f27b5d71f557b1636
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2016-12-15 18:26:51 -0800

    Adding support for more optimizers

--- language_model.py
+++ language_model.py
@@ -36,7 +36,14 @@ class LM(object):
 
         if mode == "train":
             grads = average_grads(tower_grads)
-            optimizer = tf.train.AdagradOptimizer(hps.learning_rate, initial_accumulator_value=1.0)
+             if hps.optimizer == 1:
+                    optimizer = tf.train.MomentumOptimizer(hps.learning_rate, 0.9)
+                elif hps.optimizer == 2:
+                    optimizer = tf.train.AdamOptimizer(hps.learning_rate)
+                elif hps.optimizer == 3:
+                    optimizer = tf.train.RMSPropOptimizer(learning_rate=hps.learning_rate)
+                else:
+                    optimizer = tf.train.AdagradOptimizer(hps.learning_rate, initial_accumulator_value=1.0)
             self.train_op = optimizer.apply_gradients(grads, global_step=self.global_step)
             self.summary_op = tf.summary.merge_all()
         else:
@@ -155,6 +162,7 @@ class LM(object):
             max_grad_norm=10.0,
             num_delayed_steps=150,
             keep_prob=0.9,
+            optimizer = 0,
 
             vocab_size=793470,
             emb_size=512,
