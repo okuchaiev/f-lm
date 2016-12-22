573795b1f0a0bfb591df9b03e8d46bfe2b9b9860
Oleksii Kuchaiev <okuchaiev@nvidia.com>
2016-12-22 11:59:12 -0800

    bugfix

--- run_utils.py
+++ run_utils.py
@@ -32,8 +32,7 @@ def run_train(dataset, hps, logdir, ps_device, task=0, master=""):
                              save_model_secs=120*7)
                              #save_summaries_secs=30,
                              #save_model_secs=120 * 5)
-                             
-    tf.summary.FileWriter(logdir + "/" + mode, sess.graph)
+
     config = tf.ConfigProto(allow_soft_placement=True,
                             intra_op_parallelism_threads=2,
                             inter_op_parallelism_threads=20)
